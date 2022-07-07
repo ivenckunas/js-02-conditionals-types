9046da5bb8d355b0969963f92637e60214693a80
ivenckunas <ignas.venckunas@gmail.com>
2022-07-07 11:59:33 +0300

    cao uzduotys

---
+++ js/cao-if-else.js
@@ -0,0 +1,45 @@
+// Sukurk variable 'legalAge', kuris būtų lygus 20. Antra variable 'clientAge' su bet kokiu skaičiumi. Pasirašyk if-else, kuris alert išmestų ar klientas jau pasiekęs šį legalAge, ar dar ne.
+
+// let legalAge = 20;
+// let clientAge = 5;
+
+// if (clientAge === 20 || clientAge > 20) {
+//   alert("Klientui yra daugiau nei 20m.");
+// } else {
+//   alert("Klientui dar nera 20m.");
+// }
+
+// Pasirašyk kintamą su savo vardu. Sukurk if-else, kuris pasakys "Ilgas vardas", jei tavo vardas ilgesnis nei 6 raidės. Kitu atveju, nieko neišmes. String ilgį gali susižinoti prirašant 'length' property (pvz.: "Petras".length arba su kintamu: name.length).
+
+// let vardas = "Ignas";
+// console.log("vardas ===", vardas.length);
+
+// if (vardas.length > 6) {
+//   alert("Ilgas vardas");
+// }
+
+// Sukurt kintamąjį su savo amžiumi. Patikrink: jei amžius didesnis nei 100, arba mažesnis nei 0 - tegul išmeta "Invalid age"; jei tarp 1 ir 18 - "Child"; jei tarp 19 ir 99 - "Adult".
+
+// let age = 108;
+
+// if (age > 100 || age <= 0) {
+//   alert("Invalid age");
+// } else if (age > 0 && age < 19) {
+//   alert("Child");
+// } else {
+//   alert("Adult");
+// }
+
+// Sukurk variable 'car', kuris bus lygus kažkokiam automobilio prekės ženklui. Sukurk if-else, kuris pasakys ar tas brand'as priklauso VW Group, ar BMW Group, ar nei vienam.
+// VW group priklauso: VW, Audi, Bentley, Bugatti, Lamborghini, Porsche.
+// BMW group priklauso: BMW, Mini, Rolls-Royce.
+
+// let car = "volvo";
+
+// if (car === "BMW" || car === "Mini" || car === "Rolls-Royce") {
+//   alert("Brand'as priklauso BMW group");
+// } else if (car === "VW" || car === "Audi" || car === "Bentley" || car === "Bugatti" || car === "Lamborghini" || car === "Porsche") {
+//   alert("Brand'as priklauso VW group");
+// } else {
+//   alert("Brand'as priklauso kitai group");
+// }
